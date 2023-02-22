be2ddd815f05dd57001a1271028dbb075c438cc6
Miguel Chevres <machevres6@gmail.com>
2023-02-22 01:01:25 -0400

    Enable Cors

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,20 @@ import { ValidationPipe } from '@nestjs/common';
 import { PrismaClientExceptionFilter } from './prisma-client-exception/prisma-client-exception.filter';
 
 async function bootstrap() {  
+  // Create a NestJS application instance
   const app = await NestFactory.create(AppModule);
 
+  // Enable CORS
+  app.enableCors();
+
+  // Enable global validation
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
 
+  // Enable global exception filter
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
+
+  // Enable Swagger
   const config = new DocumentBuilder()
     .setTitle('Median Clone')
     .setDescription('The Median Clone API description')
@@ -18,9 +28,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  const { httpAdapter } = app.get(HttpAdapterHost);
-  app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
-
+  // Start the server
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
